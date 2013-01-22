efb93daa5364fcf0d24e69469aa828f68913c272
Ernesta <ernesta@ernes7a.lt>
2013-01-22 10:41:20 +0100

    Better comments, better empty attribute values.

--- classes/Song.php
+++ classes/Song.php
@@ -1,21 +1,21 @@
 <?php
 	class Song {
-		//Radio-specific song ID
+		// Radio-specific song ID.
 		public $ID = "";
-		//Song's title
+		// Song's title.
 		public $title = "";
-		//Song's artist (Artist.php)
-		public $artist = null;
-		//Song's album (Album.php)
-		public $album = null;
-		//Song's tag array (Tag.php)
-		public $tags = array();
-		//Song's start timestamp
-		public $start = "";
-		//Link to song's page on Last.fm
+		// Song's artist (Artist.php).
+		public $artist = NULL;
+		// Song's album (Album.php).
+		public $album = NULL;
+		// Song's tag array (Tag.php).
+		public $tags = NULL;
+		// Song's start timestamp.
+		public $start = 0;
+		// Link to song's page on Last.fm.
 		public $URL = "";
-		//Song's Last.fm listener count
-		public $listeners = "";
-		//Song's user play count
-		public $plays = "";
+		// Song's Last.fm listener count.
+		public $listeners = 0;
+		// Song's user play count.
+		public $plays = 0;
 	}
